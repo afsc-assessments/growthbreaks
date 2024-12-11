43858e3e781afd20bd10c864be235a69f8829c5f
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2024-12-11 09:41:21 -0800

    add back in functionality

--- R/check_data.R
+++ R/check_data.R
@@ -1,20 +1,75 @@
-#' Read ADMB .rep file and return an R object of type 'list'
-#'
-#' Code modified from original function provided by Steve Martell, D'Arcy N. Webber
-
-#' called by \code{\link{read_admb_re}}
-#'
-#' @param fn full path and name of ADMB output file to be read
-#'
-#' @return object of type "list" with ADMB outputs therein
+#' load, check and plot input data for use in breakpoint detection
+#' @param dat data.frame with columns year, age, length, sex (optional)
+#' @param sex logical. does your data frame include a sex column?
+#' @param showPlot logical. do you want to visualize your input data?
+#' @return plots
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' read_rep(fn = 'inst/example_data/goasr.rep')
 #' }
-check_data <- function(fn) {
- ## ifelse for sex and colheads
-  ## check dims < 10, 5 for both
+check_data <- function(dat, sex = FALSE, showPlot = TRUE) {
+
+  ## header checks
+  colheads <- tolower(names(dat));names(dat) <- colheads
+  colneeds <- c("year","age","length",  "long" ,  "lat" )
+  if(sex){  colneeds <- c(colneeds,'sex')}
+  if(!all(colneeds %in% colheads)) stop(paste0("missing one of ",colneeds))
+  if(!sex & !all( apply(dat[,colneeds],2,is.numeric))) stop("all columns should be numeric")
+  if(sex){
+    if(!all(apply(dat[,colneeds] %>% select(-"sex"),2,is.numeric))){
+      stop("all columns besides sex should be numeric")
+    }
+  }
+
+  ## dimension checks
+  if(!sex){
+    agedims <- summarise(dat, n = n(), .by = c('age'))
+    yeardims <- summarise(dat, n = n(), .by = c('year'))
+    bothdims <- summarise(dat, n = n(), .by = c('age','year'))
+  }else{
+    agedims <- summarise(dat, n = n(), .by = c('age','sex'))
+    yeardims <- summarise(dat, n = n(), .by = c('year','sex'))
+    bothdims <- summarise(dat, n = n(), .by = c('age','year','sex'))
+  }
+
+  if(any(agedims$n< 10)) warning(paste0('low sample sizes for ages ',  unique(agedims$age[agedims$n < 10])))
+  if(any(yeardims$n< 10)) warning(paste0('low sample sizes for years ',  unique(yeardims$year[yeardims$n < 10])))
+  if(any(bothdims$n < 5)) warning(paste0('low sample sizes for years ',  unique(bothdims$year[bothdims$n < 10])))
+
+  if(showPlot){
+    dat_plot <- dat %>%
+      mutate(meanL = mean(length), .by = ifelse(sex, c('sex','age'), 'age')) %>%
+      mutate(resid = length-meanL)
+
+    ## plot raw length-age obs
+
+    p1 <- ggplot(dat_plot, aes(x = age, y = length, color = factor(year))) +
+      geom_point() +
+      theme_minimal()+
+      # guides(color = 'none')+
+      labs(x = 'Age', y = 'Length', color = 'year', title = 'Raw length-at-age observations')
+
+    ## plot raw length-age map
+    data(us)
+
+    p2 <- ggplot() +
+      geom_sf(data = us, fill = NA, color = 'black') +
+      geom_sf(data = sf_df_clipped, aes( color =resid, size = length), alpha = 0.9) +
+      # scale_y_continuous(limits = 2+c(floor(min(dat$lat)),ceiling(max(dat$lat)))) +
+      # scale_x_continuous(limits = 2+c(floor(min(dat$long)),ceiling(max(dat$long)))) +
+      scale_y_continuous(limits = c(50,71)) +
+      scale_x_continuous(limits = c(-185,-130))+
+      guides(size = 'none')+
+      theme_minimal() +
+      scale_color_gradient2(low = "blue", mid = "grey90", high = "red", midpoint = 0) +
+      labs(color = 'length residual')
+
+
+    ## plot residual map
+
+
+  }
   ## throw plots if true
 }
