fcd0dbad2d3efa38ca15db52ffb37d6ef3fa954b
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2024-12-12 12:58:40 -0800

    remove live call to tmb and update namespace

--- TMB/sptlVB_Sel_Sigma.cpp
+++ TMB/sptlVB_Sel_Sigma.cpp
@@ -1,3 +1,4 @@
+#define TMB_LIB_INIT R_init_growthbreaks
 // https://kaskr.github.io/adcomp/register_atomic_8cpp-example.html#a3
 // Similar to example 'adaptive_integration' using CppAD Romberg integration. REGISTER_ATOMIC is used to reduce tape size.
 #include <TMB.hpp>
