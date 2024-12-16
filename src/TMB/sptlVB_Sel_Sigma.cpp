052a1651c50d54650a643050df555064806dacd0
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2024-12-16 11:42:02 -0800

    rm include

--- src/TMB/sptlVB_Sel_Sigma.cpp
+++ src/TMB/sptlVB_Sel_Sigma.cpp
@@ -1,5 +1,4 @@
 #define TMB_LIB_INIT R_init_growthbreaks
-#include <TMB.hpp>
 #include <iostream>
 // https://kaskr.github.io/adcomp/register_atomic_8cpp-example.html#a3
 // Similar to example 'adaptive_integration' using CppAD Romberg integration. REGISTER_ATOMIC is used to reduce tape size.
